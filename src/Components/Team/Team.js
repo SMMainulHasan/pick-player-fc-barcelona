f737d31a2c63f9abe59bb64b9c2d7592cf9b9a93
SMMainulHasan <mainulhasan7070@gmail.com>
2021-03-01 22:39:06 +0600

    okk

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -7,13 +7,13 @@ const Team = (props) => {
     const teamCost = teamPlayer.reduce((totalSalary, player) => totalSalary + Number(player.salary), 0);
     return (
         <div className="team">
-            <h2>Team Member</h2>
+            <h2>Team Barcelona</h2>
             <h4>Team Member: {totalTeamMember}</h4>
             <p>Total Team Cost: <strong>${teamCost}</strong></p>
             <ol>
                 {teamPlayer.map(player=><li>{player.name}</li>)}
             </ol>
-            <br/>
+            <hr/>
             <p>Select total 11 players according to your need. You can choose your will play for goalkeeper, defender, midfielder and forward.Also you will get visual position of players. </p>
         </div>
     );
